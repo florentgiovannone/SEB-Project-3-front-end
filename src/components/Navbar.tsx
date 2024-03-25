9cf7f8f0bc96f11f4f17bb09a29631df806bf8ba
Dinul Haque <66052039+dinulhaque@users.noreply.github.com>
2024-03-25 12:02:46 +0000

    navbar styling and logo added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,5 @@
-import { Link, useNavigate } from "react-router-dom"
-import { IUser } from "../interfaces/user"
-
+import { Link, useNavigate } from "react-router-dom";
+import { IUser } from "../interfaces/user";
 
 interface NavbarProps {
   user: null | IUser, 
@@ -8,50 +7,74 @@ interface NavbarProps {
 }
 
 function Navbar({ user, setUser }: NavbarProps) {
-  console.log("user in the navbar: ", user)
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   function logout() {
-    localStorage.removeItem('token')
-    setUser(null)
-    navigate('/')
+    localStorage.removeItem('token');
+    setUser(null);
+    navigate('/');
   }
   
   return (
-    <>
-      <header>
-        <nav className="">
-          <div className="">
-            <div className="">
-              <Link to="/" className=" ">
-                Home
-              </Link>
-              <Link to="/wines" className=" ">
-                Winelist API
-              </Link>
-            <Link to="/aboutus" className=" ">
-                About Us
-              </Link>
-            <Link to="/contactus" className=" ">
-                Contact Us
-              </Link>
-              {user && <Link to="/create" className="">
-                Create Wines
-                          </Link>}
-            
-              {user && <button onClick={logout} className="">Logout</button>}
-              {!user && <Link to="/signup" className="">
-                Sign Up
+    <div className="has-background-white">
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand" >
+          <Link to="/" className="navbar-item">
+            <img
+              alt="Logo"
+              src="./src/assets/rougepict.png"
+              style={{
+                height: '60px',
+                width: '60px',
+                objectFit: 'cover',
+              }}
+            />
+          </Link>
+        </div>
+
+        <div className="navbar-menu">
+          <div className="navbar-start">
+            <Link to="/" className="navbar-item is-size-5 has-text-weight-semibold">
+              Home
+            </Link>
+            <Link to="/wines" className="navbar-item is-size-5 has-text-weight-semibold">
+              Wine List API
+            </Link>
+            <Link to="/aboutus" className="navbar-item is-size-5 has-text-weight-semibold">
+              About us
+            </Link>
+            <Link to="/contactus" className="navbar-item is-size-5 has-text-weight-semibold">
+              Contact us
+            </Link>
+            {user && <Link to="/create" className="navbar-item is-size-5 has-text-weight-semibold">
+              Create Wines
             </Link>}
-              {!user && <Link to="/login" className="">
-                Login
-              </Link>}
+          </div>
+
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="buttons">
+                {user ? (
+                  <button onClick={logout} className="button is-light is-size-5 has-text-weight-semibold">
+                    Logout
+                  </button>
+                ) : (
+                  <>
+                    <Link to="/signup" className="button is-light is-size-5 has-text-weight-semibold">
+                      Sign Up
+                    </Link>
+                    <Link to="/login" className="button is-light is-size-5 has-text-weight-semibold">
+                      Login
+                    </Link>
+                  </>
+                )}
+              </div>
             </div>
           </div>
-        </nav>
-      </header>
-    </>
-  )
+        </div>
+      </nav>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
