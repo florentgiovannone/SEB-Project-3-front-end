5e5e27483bf7cc397170301b4ca906f4d61e21e8
Florent Giov annone <giovannoneflorent@gmail.com>
2024-03-28 13:37:17 +0000

    fixed the merge issues

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,86 +1,102 @@
 import { Link, useNavigate } from "react-router-dom";
 import { IUser } from "../interfaces/user";
-import Logo from "../assets/images/rouge_logo.png"
+import Logo from "../assets/images/rouge_logo.png";
+import { useState } from "react";
 
 interface NavbarProps {
-  user: null | IUser, 
-  setUser: Function
+  user: null | IUser;
+  setUser: Function;
 }
 
 function Navbar({ user, setUser }: NavbarProps) {
   const navigate = useNavigate();
+  const [isActive, setIsActive] = useState(false);
+
+  function toggleNavbar() {
+    setIsActive(!isActive);
+  }
 
   function logout() {
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
     setUser(null);
-    navigate('/');
+    navigate("/");
   }
-  
+
   return (
-    <div className="has-background-white">
-      <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand" >
-          <Link to="/" >
-            <img
-            className="p-2"
-              alt="Logo"
-              src={Logo}
-              style={{
-                height: '70px',
-                width: '70px',
-                objectFit: 'cover',
-                
-              }}
-            />
+    <nav
+      className="navbar is-light"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="navbar-brand">
+        <Link to="/" className="navbar-item">
+          <img
+            src={Logo}
+            alt="Logo"
+            style={{ maxHeight: "70px", objectFit: "cover" }}
+          />
+        </Link>
+        <a
+          role="button"
+          className={`navbar-burger ${isActive ? "is-active" : ""}`}
+          aria-label="menu"
+          aria-expanded="false"
+          onClick={toggleNavbar}
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div className={`navbar-menu ${isActive ? "is-active" : ""}`}>
+        <div className="navbar-start">
+          <Link to="/" className="navbar-item">
+            Home
           </Link>
+          <Link to="/wines" className="navbar-item">
+            Wine List API
+          </Link>
+          <Link to="/aboutus" className="navbar-item">
+            About us
+          </Link>
+          <Link to="/contactus" className="navbar-item">
+            Contact us
+          </Link>
+          {user && (
+            <Link to="/create" className="navbar-item">
+              Create Wines
+            </Link>
+          )}
         </div>
 
-        <div className="navbar-menu">
-          <div className="navbar-start">
-            <Link to="/" className="navbar-item is-size-6 has-text-weight-semibold py-0 px-3">
-              Home
-            </Link>
-            <Link to="/wines" className="navbar-item is-size-6 has-text-weight-semibold py-0 px-3">
-              Wine List API
-            </Link>
-            <Link to="/aboutus" className="navbar-item is-size-6 has-text-weight-semibold py-0 px-3">
-              About us
-            </Link>
-            <Link to="/contactus" className="navbar-item is-size-6 has-text-weight-semibold py-0 px-3">
-              Contact us
-            </Link>
-            {user && <Link to="/create" className="navbar-item is-size-6 has-text-weight-semibold py-0 px-3">
-              Create Wines
-            </Link>}
-          </div>
-
-          <div className="navbar-end">
-            <div className="navbar-item py-0 px-3">
-              <div className="buttons">
-                {user ? (<>
-                  <button onClick={logout} className="button is-light is-size-6 has-text-weight-semibold">
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              {user ? (
+                <>
+                  <button onClick={logout} className="button is-light">
                     Logout
                   </button>
-                              {user && <Link to="/dashboard" className="navbar-item is-size-6 has-text-weight-semibold">
-                  Dashboard
-                  </Link>}
-                  </>
-                ) : (
-                  <>
-                    <Link to="/signup" className="button is-rouge is-light is-size-5 has-text-weight-semibold button-wrapper">
-                      Sign Up
-                    </Link>
-                    <Link to="/login" className="button is-rouge is-light is-size-5 has-text-weight-semibold button-wrapper">
-                      Login
-                    </Link>
-                  </>
-                )}
-              </div>
+                  <Link to="/dashboard" className="button is-info">
+                    Dashboard
+                  </Link>
+                </>
+              ) : (
+                <>
+                  <Link to="/signup" className="button is-primary">
+                    <strong>Sign up</strong>
+                  </Link>
+                  <Link to="/login" className="button is-light">
+                    Login
+                  </Link>
+                </>
+              )}
             </div>
           </div>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 }
 
