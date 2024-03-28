d48119a3beecc5c2138823172f97fc2dea73ea85
Florent Giov annone <giovannoneflorent@gmail.com>
2024-03-28 14:57:00 +0000

    fixed the merge issues

--- src/components/Winelist.tsx
+++ src/components/Winelist.tsx
@@ -41,8 +41,7 @@ function wineList() {
     <>
       <section className="container">
         <div className="columns is-centered ">
-          <div className="column is-half-mobile">
-
+          <div className="container ">
             <input
               className="input background-is-rouge is-rounded mt-6"
               placeholder="Search character..."
@@ -71,7 +70,7 @@ function wineList() {
               })}
             </div>
           </div>
-          </div>
+        </div>
       </section>
 
       <Footer />
