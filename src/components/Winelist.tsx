36c6cb3a1e6f508de8c84a8481158d9818b9cad8
Florent Giov annone <giovannoneflorent@gmail.com>
2024-03-28 15:01:03 +0000

    fixed the merge issues

--- src/components/Winelist.tsx
+++ src/components/Winelist.tsx
@@ -1,21 +1,20 @@
-import React from "react";
-import WineCard from "./Winecard";
-import { IWines } from "../interfaces/wine";
-import Footer from "./footer";
+import React from "react"
+import WineCard from "./Winecard"
+import { IWines } from "../interfaces/wine"
+import Footer from "./footer"
 import { baseUrl } from "../config";
 
-type Wines = null | Array<IWines>;
-
+type Wines = null | Array<IWines>
 function wineList() {
-  const [wines, setWines] = React.useState<Wines>(null);
+  const [wines, setWines] = React.useState<Wines>(null)
   React.useEffect(() => {
     async function fetchWines() {
-      const resp = await fetch(`${baseUrl}/rouge/wines`);
-      const data = await resp.json();
-      setWines(data);
+      const resp = await fetch(`${baseUrl}/rouge/wines`)
+      const data = await resp.json()
+      setWines(data)
     }
-    fetchWines();
-  }, []);
+    fetchWines()
+  }, [])
 
   const [search, setSearch] = React.useState("");
 
@@ -24,58 +23,55 @@ function wineList() {
   }
 
   function filterWines() {
-    return wines?.filter((wine: { wineName: string; winery: string }) => {
+    return wines?.filter((wine: { wineName: string, winery: string }) => {
       const lowerSearch = search.toLowerCase();
       const lowerWineName = wine.wineName.toLowerCase();
       const lowerWinery = wine.winery.toLowerCase();
 
-      return (
-        lowerWineName.includes(lowerSearch) || lowerWinery.includes(lowerSearch)
-      );
+      return lowerWineName.includes(lowerSearch) || lowerWinery.includes(lowerSearch);
     });
   }
+  const filteredLength: any = filterWines()?.length
+  
+  return (<><section className="container">
+    <div className="columns is-centered ">
+      <div className="container ">
 
-  const filteredLength: any = filterWines()?.length;
+        <input
+          className="input background-is-rouge is-rounded mt-6"
+          placeholder="Search character..."
+          onChange={handleChange}
+          value={search}
+        />
+        {filteredLength === 0 && <div className="account  has-text-centered background-is-grey mt-5">
+          <p className="text is-black ">Cannot find your wine ?</p>
+          <a href="/create"><button className="button  mb-3">Create new Wine</button></a>
 
-  return (
-    <>
-      <section className="container">
-        <div className="columns is-centered ">
-          <div className="container ">
-            <input
-              className="input background-is-rouge is-rounded mt-6"
-              placeholder="Search character..."
-              onChange={handleChange}
-              value={search}
+        </div>}
+
+        <div className="columns is-multiline is-centered">
+          {filterWines()?.map((wine) => {
+            return <WineCard
+              key={wine._id}
+              {...wine}
             />
-            {filteredLength === 0 && (
-              <div className="account  has-text-centered background-is-grey mt-5">
-                <p className="text is-black ">Cannot find your wine ?</p>
-                <a href="/create">
-                  <button className="button  mb-3">Create new Wine</button>
-                </a>
-              </div>
-            )}
-
-            <div className="columns is-multiline is-centered">
-              {filterWines()?.map((wine) => {
-                return (
-                  <div
-                    key={wine._id}
-                    className="column is-half-desktop is-full-mobile"
-                  >
-                    <WineCard {...wine} />
-                  </div>
-                );
-              })}
-            </div>
-          </div>
+          })}
         </div>
-      </section>
 
-      <Footer />
-    </>
-  );
+      </div>
+    </div>
+    <div className="container mb-6">
+      <div className="container mb-6">
+        <div className="container mb-6">
+
+        </div>
+      </div>
+      </div>
+
+
+  </section>
+
+    <Footer />
+  </>)
 }
-
-export default wineList;
+export default wineList
